3e6b210e244a70283f5c91b54eb9d2593747c6b3
Devree Lewis <devree.lewis@gmail.com>
2019-11-26 16:33:35 -0500

    first mobile commit

---
+++ index.js
@@ -0,0 +1,33 @@
+function toggleClass(){
+    let menu = document.querySelector(".navbardesktop");
+    menu.classList.toggle("toggleCls");
+}
+
+$(".toportfolio").on("click", function(event){
+        var elmnt=document.getElementById("portfolio");
+        elmnt.scrollIntoView();
+    })
+
+$(".toportfoliomenu").on("click", function(event){
+    var elmnt=document.getElementById("portfolio");
+    elmnt.scrollIntoView();
+    toggleClass();
+})
+
+$(".toaboutme").on("click", function(event){
+    var elmnt=document.getElementById("aboutme");
+    elmnt.scrollIntoView();
+    toggleClass();
+})
+
+$(".tocontact").on("click", function(event){
+    var elmnt=document.getElementById("contact");
+    elmnt.scrollIntoView();
+    toggleClass();
+})
+
+$(".logo").on("click", function(event){
+    var elmnt=document.getElementById("banner");
+    elmnt.scrollIntoView();
+})
+
