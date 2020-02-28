e6e42a2562ff024ae629c61ddb53275bd1f78351
Devree Lewis <devree.lewis@gmail.com>
2020-02-28 16:34:11 -0500

    added API to projects

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-function toggleClass(){
-    let menu = document.querySelector(".navbardesktop");
-    menu.classList.toggle("toggleCls");
+function toggleClass() {
+  let menu = document.querySelector(".navbardesktop");
+  menu.classList.toggle("toggleCls");
 }
 
-$(".toportfolio").on("click", function(event){
-        var elmnt=document.getElementById("portfolio");
-        elmnt.scrollIntoView();
-    })
+$(".toportfolio").on("click", function(event) {
+  var elmnt = document.getElementById("portfolio");
+  elmnt.scrollIntoView();
+});
 
-$(".toportfoliomenu").on("click", function(event){
-    var elmnt=document.getElementById("portfolio");
-    elmnt.scrollIntoView();
-    toggleClass();
-})
+$(".toportfoliomenu").on("click", function(event) {
+  var elmnt = document.getElementById("portfolio");
+  elmnt.scrollIntoView();
+  toggleClass();
+});
 
-$(".toaboutme").on("click", function(event){
-    var elmnt=document.getElementById("aboutme");
-    elmnt.scrollIntoView();
-    toggleClass();
-})
+$(".toaboutme").on("click", function(event) {
+  var elmnt = document.getElementById("aboutme");
+  elmnt.scrollIntoView();
+  toggleClass();
+});
 
-$(".tocontact").on("click", function(event){
-    var elmnt=document.getElementById("contact");
-    elmnt.scrollIntoView();
-    toggleClass();
-})
+$(".tocontact").on("click", function(event) {
+  var elmnt = document.getElementById("contact");
+  elmnt.scrollIntoView();
+  toggleClass();
+});
 
-$(".logo").on("click", function(event){
-    var elmnt=document.getElementById("image");
-    elmnt.scrollIntoView();
-})
-
+$(".logo").on("click", function(event) {
+  var elmnt = document.getElementById("image");
+  elmnt.scrollIntoView();
+});
